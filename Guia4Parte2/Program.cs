12a059fc196cb7469dc9d5e4ddf747be7de8658f
MatiBravo47 <matibravoneron@gmail.com>
2024-06-05 16:48:58 -0300

    Descomento ejercicios para mejorar la visualizacion

--- Guia4Parte2/Program.cs
+++ Guia4Parte2/Program.cs
@@ -19,7 +19,6 @@ namespace Guia4Parte2
              * Objetivo: El programa debe permitir al usuario ingresar numeros enteros uno por uno
              * y sumar dichos numeros hasta que el usuario decida detenerse.
             */
-            /*
             int valor, suma = 0;
             do
             {
@@ -29,13 +28,11 @@ namespace Guia4Parte2
             }
             while (valor != 0);
             Console.WriteLine($"La suma de los valores es: {suma}");
-            */
             /*
              * 2. Contar hasta un numero  
              * Objetivo: El programa debe solicitar al usuario un numero limite y luego contar 
              * desde 0 hasta ese numero, mostrando cada numero en la consola.
             */
-            /*
             int limite, contador = 0;
             Console.WriteLine("Ingrese un numero limite");
             limite = int.Parse(Console.ReadLine());
@@ -45,13 +42,11 @@ namespace Guia4Parte2
                 contador++;
             }
             while (contador <= limite);
-            */
             /*
              * 3.Convertir temperaturas 
              * Objetivo: El programa debe convertir las temperaturas de Celsius a Fahrenheit y permitir
              * al usuario realizar multiples conversiones hasta que decida deternerse.
             */
-            /*
             double celsius, fahrenheit;
             int contador = 1;
             char respuesta;
@@ -63,13 +58,11 @@ namespace Guia4Parte2
                 Console.WriteLine($"{fahrenheit}");
             }
             while (celsius != -1);
-            */
             /*
              * 4. Contador de vocales 
              * Objetivo: El programa debe contar el numero de vocales en un texto ingresado por el usuario
              * y permitirle repetir el proceso con diferentes textos.
             */
-            /*
             string cadena;
             Console.WriteLine("Ingrese texto");
             cadena = Console.ReadLine();
@@ -84,25 +77,49 @@ namespace Guia4Parte2
             }
             while (i < cadena.Length);
             Console.WriteLine($"Cantidad de vocales: {cantVocales}");
-            */
             /*
              * 5. Calculadora basica
              * Objetivo: el programa debe realizar operaciones aritmeticas basicas(suma, resta, multiplicacion y division)
              * entre dos numeros ingresados por el usuario y permitirle realizar multiples calculos.
             */
-            /*
+            int opcion;
+            double valor1, valor2;
+            char continuar;
             do
             {
+                
+                Console.WriteLine("Elija una opcion");
+                Console.WriteLine("1. Sumar");
+                Console.WriteLine("2. Restar");
+                Console.WriteLine("3. Multiplicacion");
+                Console.WriteLine("4. Division");
+                opcion = int.Parse(Console.ReadLine());
 
+                Console.WriteLine("Ingrese valor 1");
+                valor1 = double.Parse(Console.ReadLine()); 
+                Console.WriteLine("Ingrese valor 2");
+                valor2 = double.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1: Console.WriteLine($"Resultado: {valor1 + valor2}");
+                    break;
+                    case 2: Console.WriteLine($"Resultado: {valor1 - valor2}");
+                    break;
+                    case 3: Console.WriteLine($"Resultado: {valor1 * valor2}");
+                    break;
+                    case 4: Console.WriteLine($"Resultado: {valor1 / valor2}");
+                    break;
+                }
+                Console.WriteLine("Desea continuar? (s/n)");
+                continuar = Console.ReadKey().KeyChar;
             }
-            while ();
-            */
+            while (continuar == 's');
+            
             /*
              * 6. Numero par o impar
              * Objetivo: El programa debe determinar si un numero entero ingresado por el usuario 
              * es par o impar y permitirle verificar multiples numeros.
             */
-            /*
             int numero;
             Console.WriteLine("Ingrese un numero");
             numero = int.Parse(Console.ReadLine());
@@ -119,14 +136,12 @@ namespace Guia4Parte2
                 numero = int.Parse(Console.ReadLine());
             }
             while (numero != 0);
-            */
             Console.WriteLine();
             /*
              * 7. Validad edad 
              * Objetivo: El programa debe determinar si el usuario es mayor o menor de edad basado 
              * en la edad ingresada y permitirle realizar multiples verificaciones. 
             */
-            /*
             int edad;
             Console.WriteLine("Ingrese edad");
             edad = int.Parse(Console.ReadLine());
@@ -144,13 +159,11 @@ namespace Guia4Parte2
                 edad = int.Parse(Console.ReadLine());
             }
             while (edad != 0);
-            */
             /*
              * 8. Determinar la longitud de una cadena
              * Objetivo: El programa debe determinar la longitud de un texto ingresado por el usuario
              * y permitirle ingresar multiples textos 
             */
-            /*
             string texto;
             int longitud ;
             do 
@@ -161,13 +174,11 @@ namespace Guia4Parte2
                 Console.WriteLine("Longitud:" + longitud);
             }
             while (texto != "0");
-            */
             /*
              * 9. Calcular el area de un circulo
              * Objetivo: El programa debe calcular el area de un circulo basandose en el radio ingresado
              * por el usuario y permitirle realizar multiples calculos. 
             */
-            /*
             double areaCirculo, radio;
             Console.WriteLine("Ingrese el radio (0 para finalizar)");
             radio = double.Parse(Console.ReadLine());
@@ -179,13 +190,11 @@ namespace Guia4Parte2
                 radio = double.Parse(Console.ReadLine());
             }
             while (radio != 0);
-            */
             /*
              * 10. Convertir texto a mayusculas 
              * Objetivo: eL programa debe convertir un texto ingresado por el usuario a mayusculas y
              * permitirle conversiones. 
             */
-            /*
             string texto, textoEnMayus;
             Console.WriteLine("Ingrese un texto");
             texto = Console.ReadLine();
@@ -198,7 +207,6 @@ namespace Guia4Parte2
                 texto = Console.ReadLine();
             }
             while (texto != "0");
-            */
             /*
              * 11. Encontar el mayor de tres numeros 
              * Objetivo: El programa debe determinar el mayor de tres numeros ingresados por el usuario 
